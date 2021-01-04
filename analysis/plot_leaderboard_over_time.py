dcd8a7418e58175dc6c630921ff45abf979ac7ec
lintool <jimmylin@uwaterloo.ca>
2021-01-04 11:48:26 -0500

    Minor improvements to leaderboard plot.

--- analysis/plot_leaderboard_over_time.py
+++ analysis/plot_leaderboard_over_time.py
@@ -1,5 +1,7 @@
 # MS MARCO Passage: Script for plotting leaderboard over time scatter plots
 
+import datetime
+import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
 plt.switch_backend('agg')
 
@@ -9,18 +11,19 @@ df = pd.read_csv('../leaderboard/leaderboard.csv')
 df['date']= pd.to_datetime(df['date'])
 
 # Plot all the runs
-ax = df.plot(x='date',y='eval',marker='o',linestyle='none')
+ax = df.plot(x='date',y='eval',marker='o',linestyle='none',label='Submission')
 
 # Overlay all the runs that have 'BERT' in their names, in orange
 bert = df[df['description'].str.contains('BERT', case=False)]
-bert.plot(ax=ax, x='date',y='eval',marker='o',color = 'orange',linestyle='none')
+bert.plot(ax=ax, x='date',y='eval',marker='o',color = 'orange',linestyle='none',label='"BERT" technique')
 
 # Overlay all SOTA runs, in red.
 sota = df[df['Unnamed: 0'] == '🏆']
-sota.plot(ax=ax, x='date',y='eval',marker='o',color = 'red',linestyle='none')
+sota.plot(ax=ax, x='date',y='eval',marker='o',color = 'red',linestyle='none',label='SOTA')
 
-ax.get_legend().remove()
+ax.set_xlim([datetime.date(2018, 10, 1), datetime.date(2021, 1, 1)])
 
+plt.title('MS MARCO Passage Leaderboard')
 plt.xlabel('Date')
 plt.ylabel('MRR@10')
 
